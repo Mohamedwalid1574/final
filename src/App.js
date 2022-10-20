e1c94e2160a40f66da79f281e105f27647afe7f8
Mohamedwalid1574 <112758442+Mohamedwalid1574@users.noreply.github.com>
2022-10-20 16:43:13 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import Profile from './view/profile/profile'
+
 
 import './App.css';
 import Groups from './view/group/groups';
@@ -7,16 +7,11 @@ import More from './view/group/Categories';
 
 function App() {
   return (
-<<<<<<< HEAD
-   <div>
-<Profile />
 
-   </div>
-=======
     <>
       <Categories/>
     </>
->>>>>>> ef5c0eedae2f8a4a6615290a74f7d8bcf377a511
+
   );
 }
 
